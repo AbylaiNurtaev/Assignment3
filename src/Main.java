e2c2765a2356914e597851702303f58a43b865f2
ITP Project <krutyev6@gmail.com>
2024-05-14 10:21:18 +0600

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        MyHashTable hashTable = new MyHashTable();
+        MyHashTable<String, Integer> hashTable = new MyHashTable<>(5);
+        hashTable.put("one", 1);
+        hashTable.put("two", 2);
+        hashTable.put("three", 3);
 
+
+        System.out.println(hashTable.get("two"));
+
+        System.out.println(hashTable.contains(5));
+        System.out.println(hashTable.contains(2));
+
+        System.out.println(hashTable.remove("two"));
+
+        System.out.println(hashTable.getKey(3));
     }
 }
