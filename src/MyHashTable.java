209464c9e138a27e8d8a4b88341e1e4af0b05c24
ITP Project <krutyev6@gmail.com>
2024-05-14 09:31:47 +0600

    MyHashTable main part

---
+++ src/MyHashTable.java
@@ -0,0 +1,40 @@
+public class MyHashTable <K, V>{
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString(){
+            return "{" + key + " " + value + "}";
+        }
+
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11; // default number of chains
+    private int size;
+
+    public MyHashTable(){}
+
+    public MyHashTable(int M){}
+
+    public int hash(K key){
+        return 0;
+    }
+
+    public void put(K key, V value){}
+
+//    public V get(V key){return 0}
+
+//    public V remove(K key){}
+    public boolean contains(V value){return true; }
+
+//    public K getKey(V value){}
+
+}
